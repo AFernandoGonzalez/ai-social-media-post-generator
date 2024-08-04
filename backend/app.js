0f06f574e44eee86dbe726ce69aaf4281f78e76b
Fernando <ferg.developer@gmail.com>
2024-08-04 16:43:12 -0400

    added cors

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,12 @@ const setupApp = () => {
     connectDB();
 
     app.use(express.json());
-    app.use(cors());
+    app.use(cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }));
+    
 
     app.use('/api/campaigns', campaignRoutes);
     app.use('/api/topics', topicRoutes);
