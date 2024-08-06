1299d3906dc173d69b2dfa47ae369c17d3ffb4b2
Fernando <ferg.developer@gmail.com>
2024-08-06 11:59:28 -0400

    added cors

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const campaignRoutes = require('./routes/campaignRoutes');
@@ -6,27 +7,53 @@ const topicRoutes = require('./routes/topicRoutes');
 const userRoutes = require('./routes/userRoutes');
 const dotenv = require('dotenv');
 const rateLimit = require('express-rate-limit');
+const { Server } = require('socket.io');
 
 dotenv.config();
 
-const setupApp = () => {
-    const app = express();
-    connectDB();
+const app = express();
+connectDB();
 
-    const limiter = rateLimit({
-        windowMs: 15 * 60 * 1000,
-        max: 100,
-        message: 'Too many requests from this IP, please try again after 15 minutes',
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+
+app.use(express.json());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+
+app.use('/api/campaigns', campaignRoutes);
+app.use('/api/topics', topicRoutes);
+app.use('/api/users', userRoutes);
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:5173', 'https://www.quickcontentai.com'],
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('iconClicked', (icon) => {
+        io.emit('iconClicked', icon);
     });
 
-    app.use(express.json());
-    app.use(cors());
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+    });
+});
 
-    app.use('/api/campaigns', campaignRoutes);
-    app.use('/api/topics', topicRoutes);
-    app.use('/api/users', userRoutes);
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
-    return app;
-};
-
-module.exports = setupApp;
+module.exports = app;
