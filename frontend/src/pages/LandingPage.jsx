a32f3c418557eeb14c41bd753502cde7473d7be5
Fernando <ferg.developer@gmail.com>
2024-08-10 15:03:31 -0400

    added new image landing

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import './landingpage.css';
-import mic from '../assets/mic.webp';
-import bulb from '../assets/bulb.webp';
-import socialmedia from '../assets/socialmedia.webp';
-import star from '../assets/star.webp';
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./landingpage.css";
+import mic from "../assets/mic.webp";
+import brain from "../assets/brain.webp";
+import bulb from "../assets/bulb.webp";
+import socialmedia from "../assets/socialmedia.webp";
+import star from "../assets/star.webp";
+import { Link } from "react-router-dom"
 
 export const LandingPage = () => {
-
   const [liveReactions, setLiveReactions] = useState([]);
 
   const handleIconClick = (icon) => {
@@ -16,7 +17,6 @@ export const LandingPage = () => {
       setLiveReactions((prevReactions) => prevReactions.slice(1));
     }, 1000);
   };
-
 
   const variants = {
     initial: {
@@ -32,7 +32,7 @@ export const LandingPage = () => {
   };
 
   const transition = {
-    type: 'spring',
+    type: "spring",
     mass: 3,
     stiffness: 150,
     damping: 50,
@@ -45,7 +45,7 @@ export const LandingPage = () => {
 
   const specialTransition = {
     duration: 0.8,
-    ease: 'easeOut',
+    ease: "easeOut",
   };
 
   const containerVariants = {
@@ -72,7 +72,7 @@ export const LandingPage = () => {
       y: [0, -10, 0],
       transition: {
         duration: 1.5,
-        ease: 'easeInOut',
+        ease: "easeInOut",
         repeat: Infinity,
       },
     },
@@ -90,15 +90,12 @@ export const LandingPage = () => {
     initial: { opacity: 1, y: 0, zIndex: 1 },
     animate: { opacity: 0, y: -300 },
     exit: { opacity: 0 },
-    transition: { duration: 4.5, ease: 'easeOut' },
+    transition: { duration: 4.5, ease: "easeOut" },
   };
 
-
   return (
-
     <div className="flex w-full flex-col min-h-screen h-full items-center justify-center p-6 bg-black mt-14 md:mt-0">
       <div className="flex flex-col flex-grow justify-evenly w-full max-w-7xl">
-
         <motion.div
           variants={containerVariants}
           initial="initial"
@@ -111,16 +108,16 @@ export const LandingPage = () => {
             className="flex flex-col justify-center md:flex-row md:justify-between items-center flex-1 md:flex-[3_3_0%] lg:flex-[4_4_0%] px-4 md:px-6"
           >
             <div className="w-full ">
-              <h1
-                className="text-[34px] sm:text-[34px] md:text-[40px] lg:text-[50px] xl:text-[60px] leading-tight sm:leading-[1.8] md:leading-[1.8] lg:leading-[1.8] xl:leading-[1.8] font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"
-              >
+              <h1 className="text-[34px] sm:text-[34px] md:text-[40px] lg:text-[50px] xl:text-[60px] leading-tight sm:leading-[1.8] md:leading-[1.8] lg:leading-[1.8] xl:leading-[1.8] font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
                 10X Content Creation
               </h1>
 
-              <span
-                className="block mt-2 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-400 to-red-400"
-              >
-                AI <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-200 via-red-400 to-green-400">✍️/🔊</span> for Every Social Platform
+              <span className="block mt-2 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-400 to-red-400">
+                AI{" "}
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-200 via-red-400 to-green-400">
+                  ✍️/🔊
+                </span>{" "}
+                for Every Social Platform
               </span>
             </div>
           </motion.div>
@@ -139,21 +136,21 @@ export const LandingPage = () => {
               <motion.span
                 variants={softBounce}
                 className="mr-1 sm:mr-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl cursor-pointer no-select"
-                onClick={() => handleIconClick('🔥')}
+                onClick={() => handleIconClick("🔥")}
               >
                 🔥
               </motion.span>
               <motion.span
                 variants={softBounce}
                 className="mr-1 sm:mr-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl cursor-pointer no-select"
-                onClick={() => handleIconClick('❤️')}
+                onClick={() => handleIconClick("❤️")}
               >
                 ❤️
               </motion.span>
               <motion.span
                 variants={softBounce}
                 className="mr-1 sm:mr-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl cursor-pointer no-select"
-                onClick={() => handleIconClick('👍')}
+                onClick={() => handleIconClick("👍")}
               >
                 👍
               </motion.span>
@@ -164,7 +161,7 @@ export const LandingPage = () => {
                 <motion.span
                   key={index}
                   className="absolute text-2xl sm:text-3xl md:text-4xl lg:text-5xl"
-                  style={{ left: `${500}px`, bottom: '10px' }}
+                  style={{ left: `${500}px`, bottom: "10px" }}
                   variants={floatingReactionVariant}
                   initial="initial"
                   animate="animate"
@@ -176,64 +173,47 @@ export const LandingPage = () => {
             </AnimatePresence>
 
             <h3 className="text-white text-sm sm:text-base md:text-sm lg:text-sm xl:text-md mt-4  md:mt-2">
-              AI-Powered Tools to Create, Customize, and Convert Social Media Content Effortlessly
+              AI-Powered Tools to Create, Customize, and Convert Social Media
+              Content Effortlessly
             </h3>
-
           </motion.div>
         </motion.div>
-
 
         <motion.div
           initial="initial"
           animate="animate"
           className="grid w-full md:h-[60%] grid-cols-4 md:grid-cols-10"
         >
-
           <motion.div
             variants={specialVariants}
             transition={specialTransition}
-            className="col-span-4 m-2 rounded-bl-3xl md:rounded-bl-none md:col-span-6 md:ml-0 mb-0 mr-2  flex flex-col justify-center gap-2 bg-slate-800  rounded-r-3xl rounded-tl-3xl"
+            className="col-span-4 m-2 rounded-bl-3xl md:rounded-bl-none md:col-span-6 md:ml-0 mb-0 mr-2 flex flex-col justify-between gap-2 bg-slate-800 rounded-r-3xl rounded-tl-3xl p-6"
           >
-            <div className="m-4 p-4">
-              <h1
-                style={{ lineHeight: '2.5' }}
-                className="text-2xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"
+            <div className="flex flex-col md:flex-row justify-between items-center">
+              <div className="flex-1">
+                <h1
+                  style={{ lineHeight: "1.2" }}
+                  className="text-2xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 mb-2 md:mb-4"
+                >
+                  Consistent Branding
+                </h1>
+
+                <h3 className="text-lg md:text-2xl font-semibold text-white">
+                  Maintain Your Unique Voice Across All Channels with AI Precision
+                </h3>
+              </div>
+              <motion.div
+                whileHover={{ scale: 1.2 }}
+                transition={{ type: "spring", stiffness: 300 }}
+                className="flex items-center justify-center w-40 h-40 md:w-48 md:h-48 md:ml-4"
               >
-                Consistent Branding
-              </h1>
-
-              <h3 className="text-lg md:text-2xl font-semibold text-white via-pink-400 to-red-400">
-                Maintain Your Unique Voice Across All Channels with AI Precision
-              </h3>
+                <img
+                  src={bulb}
+                  alt="Audio Generation Logo"
+                  className="w-full h-full object-contain rounded-full"
+                />
+              </motion.div>
             </div>
-          </motion.div>
-
-
-
-
-
-          <motion.div
-            initial={{ opacity: 0, y: 50, scale: 0.8 }}
-            animate={{ opacity: 1, y: 0, scale: 1 }} 
-            transition={{ duration: 0.8, type: "spring", stiffness: 100 }} 
-            whileHover={{ scale: 1.05 }} 
-            className="col-span-4 rounded-3xl m-2 md:col-span-2 flex flex-col items-center justify-center  p-6 md:mb-0 text-center bg-gradient-to-b from-gray-800 via-slate-800 to-gray-900 shadow-lg"
-          >
-            <motion.div
-              whileHover={{ scale: 1.2 }}
-              transition={{ type: "spring", stiffness: 300 }}
-              className="flex items-center justify-center w-full h-[150px]"
-            >
-              <img src={mic} alt="Audio Generation Logo" className="w-[80%] h-full object-contain rounded-full" />
-            </motion.div>
-
-            <motion.h2
-              whileHover={{ scale: 1.03 }}
-              transition={{ type: "spring", stiffness: 300 }}
-              className="font-bold text-white mt-4 text-xl sm:text-2xl md:text-3xl"
-            >
-              Audio Generation
-            </motion.h2>
           </motion.div>
 
 
@@ -241,7 +221,35 @@ export const LandingPage = () => {
             initial={{ opacity: 0, y: 50, scale: 0.8 }}
             animate={{ opacity: 1, y: 0, scale: 1 }}
             transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
-            whileHover={{ scale: 1.05 }} 
+            whileHover={{ scale: 1.02 }}
+            className="col-span-4 rounded-3xl m-2 md:col-span-2 flex flex-col items-center justify-center  p-6 md:mb-0 text-center bg-gradient-to-b from-gray-800 via-slate-800 to-gray-900 shadow-lg"
+          >
+            <motion.div
+              whileHover={{ scale: 1.2 }}
+              transition={{ type: "spring", stiffness: 300 }}
+              className="flex items-center justify-center w-full h-[150px]"
+            >
+              <img
+                src={mic}
+                alt="Audio Generation Logo"
+                className="w-[80%] h-full object-contain rounded-full"
+              />
+            </motion.div>
+
+            <motion.h2
+              whileHover={{ scale: 1.03 }}
+              transition={{ type: "spring", stiffness: 300 }}
+              className="font-bold text-white mt-4"
+            >
+              Audio Generation
+            </motion.h2>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 50, scale: 0.8 }}
+            animate={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
+            whileHover={{ scale: 1.02 }}
             className="col-span-4 rounded-3xl m-2 md:col-span-2 flex flex-col items-center justify-center gap-2 bg-gradient-to-t from-gray-800 via-slate-800 to-gray-900 p-4 md:mb-0 text-center"
           >
             <motion.div
@@ -249,11 +257,15 @@ export const LandingPage = () => {
               transition={{ type: "spring", stiffness: 300 }}
               className="flex items-center justify-center w-full h-[150px]"
             >
-              <img src={socialmedia} alt="Audio Generation Logo" className="w-[80%] h-full object-contain rounded-full" />
+              <img
+                src={socialmedia}
+                alt="Audio Generation Logo"
+                className="w-[80%] h-full object-contain rounded-full"
+              />
             </motion.div>
 
             <motion.h2
-              whileHover={{ scale: 1.1, color: "#00FF00" }} 
+              whileHover={{ scale: 1.03 }}
               transition={{ type: "spring", stiffness: 300 }}
               className="font-bold text-white mt-4"
             >
@@ -265,14 +277,12 @@ export const LandingPage = () => {
   </p> */}
           </motion.div>
 
-
-
           <motion.div
             variants={specialVariants}
             transition={specialTransition}
             className="col-span-4 m-2 rounded-tl-3xl md:rounded-tl-none md:col-span-4 md:mb-2 ml-0 mt-0 flex flex-col items-center justify-center gap-2 bg-slate-800 text-white text-center rounded-bl-3xl rounded-br-3xl relative hidden md:flex custom-top-right-corner  "
           >
-            <div className=" flex p-4 rounded-lg w-full">
+            <div className="flex flex-col p-4 rounded-lg w-full">
               <div className="flex items-start m-4">
                 <div className="flex space-x-[-10px]">
                   <div className="relative w-12 h-12 rounded-full flex items-center justify-center bg-blue-600 p-0.5 border-2 border-white">
@@ -296,19 +306,36 @@ export const LandingPage = () => {
                     </div>
                   </div>
                 </div>
+
                 <div className="ml-4 text-gray-400 text-left">
                   <p>We offer many platforms</p>
                   <p>We're waiting for you</p>
                 </div>
               </div>
+
+              <motion.div
+                className="flex mt-4"
+                
+                transition={{ type: "spring", stiffness: 300 }}
+              >
+                <Link
+                  to="login"
+                  className="text-xl font-bold bg-transparent text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 py-2 px-4 rounded-full hover:opacity-80 transition duration-300 flex items-center gap-2"
+                >
+                  Get Started <i className="fas fa-arrow-right"></i>
+                </Link>
+              </motion.div>
+
+
             </div>
+
           </motion.div>
 
           <motion.div
             initial={{ opacity: 0, y: 50, scale: 0.8 }}
             animate={{ opacity: 1, y: 0, scale: 1 }}
             transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
-            whileHover={{ scale: 1.05 }} 
+            whileHover={{ scale: 1.02 }}
             className="col-span-4 rounded-3xl m-2 md:col-span-4 flex flex-col items-center justify-center gap-2 bg-gradient-to-r from-gray-800 via-slate-800 to-gray-900 p-4 md:mt-4  text-center rounded-3xl"
           >
             <motion.div
@@ -316,12 +343,20 @@ export const LandingPage = () => {
               transition={{ type: "spring", stiffness: 300 }}
               className="flex items-center justify-center w-full h-[150px]"
             >
-              <img src={bulb} alt="Audio Generation Logo" className="w-[80%] h-full object-contain rounded-full" />
+              <img
+                src={brain}
+                alt="Audio Generation Logo"
+                className="w-[80%] h-full object-contain rounded-full"
+              />
             </motion.div>
 
-            <h2 className="font-bold text-white mt-4">
+            <motion.h2
+              whileHover={{ scale: 1.03 }}
+              transition={{ type: "spring", stiffness: 300 }}
+              className="font-bold text-white mt-4"
+            >
               AI-Driven Insights
-            </h2>
+            </motion.h2>
 
             {/* <p className="text-center text-white text-lg">
               Harness AI to gain deep insights and optimize your social media strategy.
@@ -332,7 +367,7 @@ export const LandingPage = () => {
             initial={{ opacity: 0, y: 50, scale: 0.8 }}
             animate={{ opacity: 1, y: 0, scale: 1 }}
             transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
-            whileHover={{ scale: 1.05 }} 
+            whileHover={{ scale: 1.02 }}
             className="col-span-4 rounded-3xl m-2 md:col-span-2 flex flex-col items-center justify-center gap-2 bg-gradient-to-l from-gray-800 via-slate-800 to-gray-900 p-4  md:mt-4 text-center rounded-3xl"
           >
             <motion.div
@@ -340,25 +375,27 @@ export const LandingPage = () => {
               transition={{ type: "spring", stiffness: 300 }}
               className="flex items-center justify-center w-full h-[150px]"
             >
-              <img src={star} alt="Audio Generation Logo" className="w-[80%] h-full object-contain rounded-full" />
+              <img
+                src={star}
+                alt="Audio Generation Logo"
+                className="w-[80%] h-full object-contain rounded-full"
+              />
             </motion.div>
 
-            <h2 className="font-bold text-white mt-4">
+            <motion.h2
+              whileHover={{ scale: 1.03 }}
+              transition={{ type: "spring", stiffness: 300 }}
+              className="font-bold text-white mt-4"
+            >
               Creative Spark
-            </h2>
+            </motion.h2>
 
             {/* <p className="text-center text-white text-lg">
               Get instant AI-powered inspiration for engaging content ideas.
             </p> */}
           </motion.div>
-
-
         </motion.div>
-
-
       </div>
     </div>
   );
 };
-
-
