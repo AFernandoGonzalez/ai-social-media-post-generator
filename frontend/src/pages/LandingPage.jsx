f8ea6db22d0564207da145345bb9a7c8fbae4810
Fernando <ferg.developer@gmail.com>
2024-08-10 10:03:56 -0400

    Updated Landing page to be resposinve in progress...

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import './landingpage.css';
 
@@ -10,7 +10,7 @@ export const LandingPage = () => {
     setLiveReactions((prevReactions) => [...prevReactions, icon]);
     setTimeout(() => {
       setLiveReactions((prevReactions) => prevReactions.slice(1));
-    }, 1000); 
+    }, 1000);
   };
 
 
@@ -65,7 +65,7 @@ export const LandingPage = () => {
 
   const softBounce = {
     animate: {
-      y: [0, -10, 0], 
+      y: [0, -10, 0],
       transition: {
         duration: 1.5,
         ease: 'easeInOut',
@@ -84,46 +84,46 @@ export const LandingPage = () => {
 
   const floatingReactionVariant = {
     initial: { opacity: 1, y: 0, zIndex: 1 },
-    animate: { opacity: 0, y: -300 }, 
+    animate: { opacity: 0, y: -300 },
     exit: { opacity: 0 },
-    transition: { duration: 4.5, ease: 'easeOut' }, 
+    transition: { duration: 4.5, ease: 'easeOut' },
   };
 
 
   return (
-    <div className="flex w-full flex-col h-screen items-center justify-center p-6 overflow-auto bg-black ">
+    <div className="flex w-full flex-col h-full md:h-screen items-center justify-center p-6  bg-black  mt-14 md:mt-1">
       <div className="h-full w-full flex flex-col align-center justify-evenly max-w-7xl">
+
         <motion.div
           variants={containerVariants}
           initial="initial"
           animate="animate"
-          className="grid w-full h-40 grid-cols-10"
+          className="w-full h-full md:h-40 flex flex-col lg:flex-row justify-between"
         >
           <motion.div
             variants={textVariants}
             transition={transition}
-            className="col-span-8 flex justify-between items-center"
+            className="flex flex-col justify-center md:flex-row md:justify-between items-center flex-1 md:flex-[3_3_0%] lg:flex-[4_4_0%] px-4 md:px-6"
           >
-            <div className="ml-4 text-left">
+            <div className="w-full ">
               <h1
-                style={{ lineHeight: '1.5' }}
-                className="text-3xl md:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"
+                className="text-[34px] sm:text-[34px] md:text-[40px] lg:text-[50px] xl:text-[60px] leading-tight sm:leading-[1.8] md:leading-[1.8] lg:leading-[1.8] xl:leading-[1.8] font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"
               >
                 10X Content Creation
               </h1>
+
               <span
-                className="text-2xl md:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-400 to-red-400"
+                className="block mt-2 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-400 to-red-400"
               >
                 AI <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-200 via-red-400 to-green-400">✍️/🔊</span> for Every Social Platform
               </span>
-
             </div>
           </motion.div>
 
           <motion.div
             variants={textVariants}
             transition={transition}
-            className="col-span-2 justify-end flex flex-col "
+            className="flex flex-col items-start  justify-start lg:justify-end   flex-1 md:flex-[1_1_0%] lg:flex-[2_2_0%] px-4 md:px-6"
           >
             <motion.div
               variants={containerVariants}
@@ -133,21 +133,21 @@ export const LandingPage = () => {
             >
               <motion.span
                 variants={softBounce}
-                className="mr-2 text-4xl cursor-pointer no-select"
+                className="mr-1 sm:mr-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl cursor-pointer no-select"
                 onClick={() => handleIconClick('🔥')}
               >
                 🔥
               </motion.span>
               <motion.span
                 variants={softBounce}
-                className="mr-2 text-4xl cursor-pointer no-select"
+                className="mr-1 sm:mr-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl cursor-pointer no-select"
                 onClick={() => handleIconClick('❤️')}
               >
                 ❤️
               </motion.span>
               <motion.span
                 variants={softBounce}
-                className="mr-2 text-4xl cursor-pointer no-select"
+                className="mr-1 sm:mr-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl cursor-pointer no-select"
                 onClick={() => handleIconClick('👍')}
               >
                 👍
@@ -158,8 +158,8 @@ export const LandingPage = () => {
               {liveReactions.map((icon, index) => (
                 <motion.span
                   key={index}
-                  className="absolute  text-4xl"
-                  style={{ left: `${500}px`, bottom: '10px' }} 
+                  className="absolute text-2xl sm:text-3xl md:text-4xl lg:text-5xl"
+                  style={{ left: `${500}px`, bottom: '10px' }}
                   variants={floatingReactionVariant}
                   initial="initial"
                   animate="animate"
@@ -170,21 +170,23 @@ export const LandingPage = () => {
               ))}
             </AnimatePresence>
 
-            <h3 className="text-white text-xl md:text-lg">
+            <h3 className="text-white text-sm sm:text-base md:text-sm lg:text-sm xl:text-md mt-4 md:mt-2">
               AI-Powered Tools to Create, Customize, and Convert Social Media Content Effortlessly
             </h3>
+
           </motion.div>
         </motion.div>
+
 
         <motion.div
           initial="initial"
           animate="animate"
-          className="grid w-full h-[60%] grid-cols-4 md:grid-cols-10"
+          className="grid w-full md:h-[60%] grid-cols-4 md:grid-cols-10"
         >
           <motion.div
             variants={specialVariants}
             transition={specialTransition}
-            className="col-span-4 md:col-span-6 flex flex-col justify-center gap-6 bg-slate-800 text-white mr-2 rounded-r-3xl rounded-tl-3xl"
+            className="col-span-4 m-2 rounded-bl-3xl md:rounded-bl-none md:col-span-6 md:m-0 flex flex-col justify-center gap-6 bg-slate-800 mr-2 rounded-r-3xl rounded-tl-3xl"
           >
             <div className="m-4 p-4">
               <h1
@@ -193,6 +195,7 @@ export const LandingPage = () => {
               >
                 Consistent Branding
               </h1>
+
               <h3 className="text-lg md:text-2xl font-semibold text-white via-pink-400 to-red-400">
                 Maintain Your Unique Voice Across All Channels with AI Precision
               </h3>
@@ -202,25 +205,47 @@ export const LandingPage = () => {
           <motion.div
             variants={variants}
             transition={transition}
-            className="col-span-4 md:col-span-2 flex flex-col items-center justify-center gap-6 bg-green-400 text-white p-4 md:p-6 text-center rounded-3xl mr-2"
+            className="col-span-4 rounded-3xl m-2 md:col-span-2 flex flex-col items-center justify-center gap-2 bg-gradient-to-r from-blue-400 via-teal-500 to-green-400 p-4 md:p-6 text-center rounded-3xl mr-2"
           >
-            <h2 className="text-2xl font-bold mb-2">Audio Generation</h2>
-            <p className="text-center text-lg">Convert your text into engaging audio posts with AI.</p>
+            <div className="flex items-center justify-center w-16 h-16 bg-white rounded-full">
+            
+              <i className="fas fa-microphone-alt text-4xl text-blue-600"></i>
+            </div>
+
+            <h2 className="font-bold text-white mt-4">
+              Audio Generation
+            </h2>
+
+            <p className="text-center text-white text-lg">
+              Convert your text into engaging audio posts with AI.
+            </p>
           </motion.div>
+
 
           <motion.div
             variants={variants}
             transition={transition}
-            className="col-span-4 md:col-span-2 flex flex-col items-center justify-center gap-6 bg-blue-400 text-white p-4 md:p-6 text-center rounded-3xl"
+            className="col-span-4 rounded-3xl m-2 md:col-span-2 flex flex-col items-center justify-center gap-2 bg-gradient-to-r from-indigo-500 via-blue-400 to-teal-400 p-4 md:p-6 text-center rounded-3xl"
           >
-            <h2 className="text-2xl font-bold mb-2">Global Reach</h2>
-            <p className="text-center text-lg">Expand your influence globally with multi-language AI content.</p>
+            <div className="flex items-center justify-center w-16 h-16 bg-white rounded-full">
+            
+              <i className="fas fa-globe text-4xl text-indigo-600"></i>
+            </div>
+
+            <h2 className="font-bold text-white mt-4">
+              Global Reach
+            </h2>
+
+            <p className="text-center text-white text-lg">
+              Expand your influence globally with multi-language AI content.
+            </p>
           </motion.div>
+
 
           <motion.div
             variants={specialVariants}
             transition={specialTransition}
-            className="col-span-4 md:col-span-4 flex flex-col items-center justify-center gap-6 bg-slate-800 text-white p-4 md:p-6 text-center rounded-bl-3xl rounded-br-3xl relative custom-top-right-corner"
+            className="col-span-4 m-2 rounded-tl-3xl md:rounded-tl-none md:col-span-4 md:m-0 flex flex-col items-center justify-center gap-6 bg-slate-800 text-white p-4 md:p-6 text-center rounded-bl-3xl rounded-br-3xl relative hidden md:block custom-top-right-corner"
           >
             <div className="flex p-4 rounded-lg w-full">
               <div className="flex items-start">
@@ -257,21 +282,46 @@ export const LandingPage = () => {
           <motion.div
             variants={variants}
             transition={transition}
-            className="col-span-4 md:col-span-4 flex flex-col items-center justify-center gap-6 bg-purple-400 text-white p-4 md:p-6 text-center rounded-3xl mt-2 ml-2 mr-2"
+            className="col-span-4 rounded-3xl m-2 md:col-span-4 flex flex-col items-center justify-center gap-6 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-4 md:p-6 text-center rounded-3xl"
           >
-            <h2 className="text-2xl font-bold mb-2">AI-Driven Insights</h2>
-            <p className="text-center text-lg">Harness AI to gain deep insights and optimize your social media strategy.</p>
+            <div className="flex items-center justify-center w-16 h-16 bg-white rounded-full">
+           
+              <i className="fas fa-lightbulb text-4xl text-purple-600"></i>
+            </div>
+
+            <h2 className="font-bold text-white mt-4">
+              AI-Driven Insights
+            </h2>
+
+            <p className="text-center text-white text-lg">
+              Harness AI to gain deep insights and optimize your social media strategy.
+            </p>
           </motion.div>
+
 
           <motion.div
             variants={variants}
             transition={transition}
-            className="col-span-4 md:col-span-2 flex flex-col items-center justify-center gap-6 bg-yellow-400 text-white text-center rounded-3xl mt-2"
+            className="col-span-4 rounded-3xl m-2 md:col-span-2 flex flex-col items-center justify-center gap-2 bg-gradient-to-r from-green-400 via-teal-500 to-blue-500 p-4 md:p-6 text-center rounded-3xl"
           >
-            <h2 className="text-2xl font-bold mb-2">Creative Spark</h2>
-            <p className="text-center text-lg">Get instant AI-powered inspiration for engaging content ideas.</p>
+            <div className="flex items-center justify-center w-16 h-16 bg-white rounded-full">
+           
+              <i className="fas fa-star text-4xl text-green-600"></i>
+            </div>
+
+            <h2 className="font-bold text-white mt-4">
+              Creative Spark
+            </h2>
+
+            <p className="text-center text-white text-lg">
+              Get instant AI-powered inspiration for engaging content ideas.
+            </p>
           </motion.div>
+
+
         </motion.div>
+
+
       </div>
     </div>
   );
